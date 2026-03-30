f5dc56fda1bb792eb4ef3355809ca2776168a0a1
root <root@vmi3186223.contaboserver.net>
2026-03-30 15:32:52 +0200

    fix: run Electron main/preload in CommonJS for dev

---
+++ electron/main.cjs
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+    win.webContents.openDevTools({ mode: 'detach' });
+  } else {
+    // TODO: load production build
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
